84c5bda0135928730d26da809a01a532cac578e3
konbraphat51 <konbraphat@gmail.com>
2023-09-05 18:44:40 +0900

    adjust using consts

--- YoutubeTranslator/VideoGetter.py
+++ YoutubeTranslator/VideoGetter.py
@@ -2,10 +2,11 @@ import pytube
 from YoutubeTranslator.Utils import Consts
 
 class VideoGetter:
-    def __init__(self):
+    def __init__(self, consts: Consts):
         self.file_name = "original.mp4"
+        self.consts = consts
     
-    def run(self, url):
+    def run(self, url: str):
         '''
         input: url of youtube video to process
         output: file name of downloaded video
@@ -16,7 +17,7 @@ class VideoGetter:
         
     def download(self, url):
         yt = pytube.YouTube(url)
-        return yt.streams.filter(file_extension='mp4').first().download(filename=self.file_name)
+        return yt.streams.filter(file_extension='mp4').first().download(filename=self.consts.cache_directory / self.file_name)
     
 if __name__ == "__main__":
-    VideoGetter().run("https://www.youtube.com/watch?v=ThhhNAMaJcw")
+    VideoGetter(Consts()).run("https://www.youtube.com/watch?v=ThhhNAMaJcw")
