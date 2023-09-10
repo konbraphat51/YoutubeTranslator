46c5f2f9f793cab61f1927c94c44a4f05208fd80
konbraphat51 <konbraphat@gmail.com>
2023-09-10 20:08:13 +0900

    add submodule

--- YoutubeTranslator/VoiceTranslator.py
+++ YoutubeTranslator/VoiceTranslator.py
@@ -1,5 +1,13 @@
 from YoutubeTranslator.Utils import Consts
+from VALLEX.utils.prompt_making import make_prompt
 
 class VoiceTranslator:
+    '''
+    Translate voice data according to the translated text data.
+    '''
+    
     def __init__(self, consts: Consts):
         self.consts = consts
+        
+    def run(self):
+        pass
